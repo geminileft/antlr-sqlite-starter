7b9976310e416f1cc4e216f8607ec65cbf18aa58
Matt Galaviz <matthew.galaviz@gmail.com>
2019-07-14 19:44:34 -0700

    Initial: Initial commit for repo.

---
+++ src/main/java/MainEntry.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+public class MainEntry {
+
+	private final static Logger logger = Logger.getLogger(MainEntry.class);
+	
+	public static void main(String[] args) {
+		logger.info("Starting MainEntry");
+
+	}
+
+}
